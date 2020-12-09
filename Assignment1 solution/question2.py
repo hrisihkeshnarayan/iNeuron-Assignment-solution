c2a2373e018948bf8106fc97cc59160f96a30407
hrisihkeshnarayan <73737410+hrisihkeshnarayan@users.noreply.github.com>
2020-12-09 13:35:35 +0530

    Create question2.py

---
+++ Assignment1 solution/question2.py
@@ -0,0 +1,10 @@
+###Write a Python program to accept the user's first and last name and then getting them printed in the the reverse order with a space between first name and last name.
+
+First_name = input("Enter First name : ")
+Last_name = input("Enter last name :")
+print(First_name[::-1] +" "+Last_name[::-1])
+
+##OUTPUT ==
+## Enter First name : hrishikesh
+##Enter last name :narayan
+##hsekihsirh nayaran
